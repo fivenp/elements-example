ad6eef64e0fd47e4740baeb24f0e99e3bd98d860
kialysa <kialysa@yahoo.com>
2017-11-22 17:31:28 +0100

    initial commit

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { ColorPalette } from '@allthings/colors'
 import Card from '@allthings/elements/molecules/Card'
-import { List } from '@allthings/elements/molecules/List'
 import Inset from '@allthings/elements/atoms/Inset'
 import TitleBar from '@allthings/elements/organisms/TitleBar'
 import Text from '@allthings/elements/atoms/Text'
 import ThemeProvider from '@allthings/elements/behaviour/ThemeProvider'
 import TodoItem from './TodoItem'
+import ResourceProvider from '@allthings/elements/behaviour/ResourceProvider'
+import View from '@allthings/elements/atoms/View'
+import TextInput from '@allthings/elements/molecules/TextInput'
+import { css } from 'glamor'
+import CardList from '@allthings/elements/organisms/CardList/CardList'
+import Icon from '@allthings/elements/atoms/Icon'
+import GroupedCardList from '@allthings/elements/organisms/CardList/GroupedCardList'
+import Button from '@allthings/elements/molecules/Button'
 
 const DemoTheme = {
   primary: ColorPalette.red,
@@ -21,54 +29,180 @@ const DemoTheme = {
 class App extends Component {
   state = {
     todos: [
-      { id: 1, text: 'Search for cocktail recipes for the party', done: false },
-      { id: 2, text: 'Create nice invitation cards', done: false },
-      { id: 3, text: 'Ask some people to bring some finger food', done: false }
-    ]
+      { id: 0, text: 'Search for cocktail recipes for the party', done: false, doubleClicked: false },
+      { id: 1, text: 'Create nice invitation cards', done: false, doubleClicked: false },
+      { id: 2, text: 'Ask some people to bring some finger food', done: false, doubleClicked: false }
+    ],
+    filter: 'all'
   }
 
-  handleClick = id => {
-    this.setState(({ todos }) => ({
-      todos: todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            done: !todo.done
-          }
-        } else {
-          return todo
-        }
-      })
-    }))
+  handleRemove = id => {
+    console.log('id is', this)
+    const newTodo = this.state.todos.slice()
+    newTodo.splice(id, 1)
+    this.setState({ todos: newTodo })
+  }
+
+  handleDoubleClick = id => {
+    const newTodo = this.state.todos.slice()
+    newTodo[id].doubleClicked = true
+
+    this.setState({ todos: newTodo })
+  }
+
+  doneEditting = (event, id) => {
+    const text = event.target.value
+    if (event.key === 'Enter' && text) {
+      const newTodo = this.state.todos.slice()
+      newTodo[id].doubleClicked = false
+      newTodo[id].text = text
+
+      this.setState({ todos: newTodo })
+    }
+  }
+
+  checkmarkClicked = id => {
+    console.log(id)
+    const newTodo = this.state.todos.slice()
+    newTodo[id].done = !this.state.todos[id].done
+    this.setState({ todos: newTodo })
+  }
+
+  handleKeyPress = event => {
+    const text = event.target.value
+    if (event.key === 'Enter' && text) {
+      const newTodo = this.state.todos.slice()
+      const item = {
+        id: newTodo.length,
+        text,
+        done: false
+      }
+      newTodo.push(item)
+      this.setState({ todos: newTodo })
+      document.getElementById('new').value = ''
+    }
+  }
+
+  changeFilter = event => {
+    const id = event.target.id
+    this.setState({ filter: id })
   }
 
   render() {
+    console.log(this.state)
+    let styles = {
+      create: css({
+        padding: '5px',
+        margin: '3px',
+        float: 'right'
+      }),
+      inset: css({
+        width: '95%'
+      }),
+      new: css({
+        padding: 0,
+        width: '100%'
+      }),
+      iconStyle: css({
+        display: 'inline-block',
+        marginRight: '10px',
+        position: 'absolute'
+      }),
+      titleBar: css({
+        padding: '10px 15px 10px 0px'
+      }),
+      titleText: css({
+        display: 'inline-block',
+        padding: '0 35px',
+        position: 'relative'
+      }),
+      buttonDiv: css({
+        textAlign: 'center'
+      }),
+      button: css({
+        margin: '3px'
+      })
+    }
+
     return (
       <div className="App">
-        <ThemeProvider theme={DemoTheme}>
-          <div>
-            <TitleBar>
-              <Inset>
-                <Text size="m" color="contrast">
-                  Todo
-                </Text>
-              </Inset>
-            </TitleBar>
-            <Card>
-              <List>
-                {this.state.todos.map(todo => (
-                  <TodoItem
-                    onClick={this.handleClick}
-                    id={todo.id}
-                    done={todo.done}
-                  >
-                    {todo.text}
-                  </TodoItem>
-                ))}
-              </List>
-            </Card>
-          </div>
-        </ThemeProvider>
+        <ResourceProvider>
+          <ThemeProvider theme={DemoTheme}>
+            <View>
+              <TitleBar {...styles.titleBar}>
+                <Inset {...styles.inset}>
+                  <Icon name="book-open" size="m" color="white" {...styles.iconStyle} />
+                  <Text color="contrast" {...styles.titleText}>My Todos</Text>
+                </Inset>
+                <Icon name="plus-light" size="m" color="#FFFFFF" />
+              </TitleBar>
+              <Card>
+                <div {...styles.new}> 
+                  <TextInput id="new" name="new" placeholder="Add new" onKeyPress={this.handleKeyPress} />
+                </div>
+                <div {...styles.buttonDiv}>
+                  <Button id="all" {...styles.button} onClick={this.changeFilter}><span id="all">All</span></Button>
+                  <Button id="incomplete" onClick={this.changeFilter} backgroundColor="rgba(232, 76, 61, 0.5)" {...styles.button} {...styles.button}><span id="incomplete">Incomplete</span></Button>
+                  <Button id="completed" onClick={this.changeFilter} backgroundColor="rgba(232, 76, 61, 0.5)" {...styles.button}><span id="completed">Completed</span></Button>
+                </div>
+                <GroupedCardList>
+                  {this.state.todos.map((todo, index) => {
+                    if (this.state.filter === "incomplete") {
+                      if (todo.done == false) {
+                        return (
+                          <TodoItem
+                            handleRemove={this.handleRemove}
+                            id={index}
+                            done={todo.done}
+                            key={'checkbox' + index}
+                            onDoubleClick={this.handleDoubleClick}
+                            doubleClicked={todo.doubleClicked}
+                            doneEditting={this.doneEditting}
+                            checkmarkClicked={this.checkmarkClicked}
+                          >
+                            {todo.text}
+                          </TodoItem>
+                        )
+                      }
+                    } else if (this.state.filter === "completed") {
+                      if (todo.done == true) {
+                        return (
+                          <TodoItem
+                            handleRemove={this.handleRemove}
+                            id={index}
+                            done={todo.done}
+                            key={'checkbox' + index}
+                            onDoubleClick={this.handleDoubleClick}
+                            doubleClicked={todo.doubleClicked}
+                            doneEditting={this.doneEditting}
+                            checkmarkClicked={this.checkmarkClicked}
+                          >
+                            {todo.text}
+                          </TodoItem>
+                        )
+                      }
+                    } else if (this.state.filter === "all") {
+                      return (
+                        <TodoItem
+                          handleRemove={this.handleRemove}
+                          id={index}
+                          done={todo.done}
+                          key={'checkbox' + index}
+                          onDoubleClick={this.handleDoubleClick}
+                          doubleClicked={todo.doubleClicked}
+                          doneEditting={this.doneEditting}
+                          checkmarkClicked={this.checkmarkClicked}
+                        >
+                          {todo.text}
+                        </TodoItem>
+                      )
+                    }
+                  })}
+                </GroupedCardList>
+              </Card>
+            </View>
+          </ThemeProvider>
+        </ResourceProvider>
       </div>
     )
   }
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Checkmark from '@allthings/elements/molecules/Checkmark'
-import { ListItem } from '@allthings/elements/molecules/List'
 import Text from '@allthings/elements/atoms/Text'
-import Inset from '@allthings/elements/atoms/Inset'
+import ListItem from '@allthings/elements/molecules/List/ListItem'
+import TextInput from '@allthings/elements/molecules/TextInput'
+import Icon from '@allthings/elements/atoms/Icon'
+import Checkmark from '@allthings/elements/molecules/Checkmark'
+import ColorPalette from '@allthings/colors/lib/ColorPalette'
+import Button from '@allthings/elements/molecules/Button'
+import { css } from 'glamor'
 
 class TodoItem extends React.Component {
   static propTypes = {
@@ -12,16 +16,84 @@ class TodoItem extends React.Component {
     done: PropTypes.bool.isRequired
   }
 
-  handleClick = () => this.props.onClick(this.props.id)
+  state = {
+    display: 'none',
+  }
+
+  handleRemove = () => this.props.handleRemove(this.props.id)
+
+  handleDoubleClick = () => this.props.onDoubleClick(this.props.id)
+
+  handleEditComplete = event => this.props.doneEditting(event, this.props.id)
+
+  handleCheckClick = event => {
+    this.props.checkmarkClicked(this.props.id)
+  }
+
+  editTask = () => {
+    this.props.onDoubleClick(this.props.id)
+  }
+
+  handleMouseOver = () => {
+    this.setState({ display: 'inline' })
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ display: 'none' })
+  }
 
   render() {
+    const styles = {
+      list: css({
+        display: 'inline-flex'
+      }),
+      icons: css({
+        width: '8%'
+      }),
+      listItem: css({
+        'minHeight': '50px',
+        width: '100%'
+      }),
+      task: css({
+        display: 'inline-flex',
+        width: '92%'
+      }),
+      text: css({
+        padding: '10px 10px 15px 10px',
+        width: '100%'
+      }),
+      remove: css({
+        display: this.state.display,
+        float: 'right',
+        padding: '5px',
+        margin: '3px',
+        ':hover': { cursor: 'pointer' }
+      }),
+      edit: css({
+        float: 'left',
+        padding: '5px',
+        margin: '3px',
+        ':hover': {
+          cursor: 'pointer'
+        }
+      })
+    }
+    const { id, children, done, doubleClicked } = this.props
+
     return (
-      <ListItem onClick={this.handleClick}>
-        <Checkmark checked={this.props.done} />
-        <Inset>
-          <Text>{this.props.children}</Text>
-        </Inset>
-      </ListItem>
+      <div {...styles.list} onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
+        <ListItem {...styles.listItem}>
+          <div {...styles.task}>
+            <Checkmark checked={this.props.done} onClick={this.handleCheckClick} {...styles.check} />
+            <div {...styles.text} onDoubleClick={this.handleDoubleClick}>
+              {doubleClicked ? <TextInput id={id} name="edit" defaultValue={children} placeholder="Edit todo" onKeyPress={this.handleEditComplete}/> : <Text autoBreak={true} id={id}>{children}</Text>}
+            </div>
+          </div>
+        <div {...styles.icons}>
+          <Icon name="remove-light-filled" {...styles.remove} size="m" onClick={this.handleRemove} />
+        </div>
+        </ListItem>
+      </div>
     )
   }
 }
